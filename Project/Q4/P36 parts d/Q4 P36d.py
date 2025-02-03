6e468a2773f5d9e53867ab1269c72f18c45db8e6
Kaushik P Palavalasa <94793173+kapa2006@users.noreply.github.com>
2025-02-03 13:10:47 +0530

    Project upload
    A project that graphs the path, momentum and energy of the Voyager as it slingshotted around Jupiter.
    
    Also includes some interesting three body simulations.

---
+++ Project/Q4/P36 parts d/Q4 P36d.py
@@ -0,0 +1,178 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as const
+import numpy.linalg as alg
+from time import perf_counter
+from matplotlib import animation
+from pyfonts import load_font
+import sys
+# Setting font for the plot
+cm = load_font(font_path="/Users/mav/Onedrive/026 Fonts/cm-unicode-0.7.0/cmunrm.ttf")
+
+
+# Defining G the Gravitational Constant
+G = const.G
+
+# final time and change in time
+frames = 480
+t = 3.25e5
+delta_t = 6.5
+
+# Masses in our three body problem
+
+m1 = 0
+m2 = 1.898e27
+m3 = 722
+
+
+# Arrays for 1, 2, and 3 positions + defining the intial positions
+
+positions_1 = np.ones([3,int(t/delta_t)+2])
+positions_2 = np.ones([3,int(t/delta_t)+2])
+positions_3 = np.ones([3,int(t/delta_t)+2])
+positions_1[0:3,0] = [0,0,0]
+positions_2[0:3,0] = [7.405e11,0,0]
+positions_3[0:3,0] = [7.405e11+0.265e9,-1.7873e9,0]
+initial_diff = alg.norm(np.subtract(positions_3[:,0], positions_2[:,0]))
+
+# Arrays for 1, 2, and 3 velocities + defining the initial velocities
+
+velocities_1 = np.ones([3,int(t/delta_t)+2])
+velocities_2 = np.ones([3,int(t/delta_t)+2])
+velocities_3 = np.ones([3,int(t/delta_t)+2])
+velocities_1[0:3,0] = [0,0,0]
+velocities_2[0:3,0] = [0,-13070,0]
+velocities_3[0:3,0] = [1e4,0,0]
+
+# Arrays for 1, 2, and 3 accelerations + defining the initial accelerations (if any)
+
+accelerations_1 = np.zeros([3,int(t/delta_t)+1])
+accelerations_2 = np.zeros([3,int(t/delta_t)+1])
+accelerations_3 = np.zeros([3,int(t/delta_t)+1])
+
+# Array for time 
+
+time_array = np.linspace(0, t, (int(t/delta_t))+1, True)
+
+
+# We can define constants for everything that doesn't change, particularly  G*m1, G*m2, G*m3
+
+constant_m1 = G*m1
+constant_m2 = G*m2
+constant_m3 = G*m3
+
+# 4th order runge-kutte method for velocity, and position
+
+def avg_slope(k_1, k_2, k_3, k_4):
+	return (k_1 + 2*k_2 + 2*k_3 + k_4)/6
+
+def odesystem_vel(const_1,const_2,posofmass,position_1,position_2):
+	return (const_1/((alg.norm(np.subtract(position_1,posofmass)))**3))*np.subtract(position_1,posofmass)+(const_2/((alg.norm(np.subtract(position_2,posofmass)))**3))*np.subtract(position_2,posofmass)
+
+
+cur_col = 0
+speed_probe = []
+for i in time_array:
+	cur_pos_1 = positions_1[0:3,cur_col]
+	cur_pos_2 = positions_2[0:3,cur_col]
+	cur_pos_3 = positions_3[0:3,cur_col]
+	cur_vel_1 = velocities_1[0:3,cur_col]
+	cur_vel_2 = velocities_2[0:3,cur_col]
+	cur_vel_3 = velocities_3[0:3,cur_col] 
+	cur_probe_speed = alg.norm(cur_vel_3)
+	speed_probe.append(cur_probe_speed)
+	if (alg.norm(np.subtract(cur_pos_3,cur_pos_2)))<= 6.9e7:
+		print("Oops looks like Voyager crashed into Jupiter")
+		sys.exit()
+	# Finding estimated accelerations (k1s) for the three objects
+	a_1_1 = odesystem_vel(constant_m2, constant_m3,cur_pos_1,cur_pos_2,cur_pos_3)
+	a_1_2 = odesystem_vel(constant_m1, constant_m3,cur_pos_2,cur_pos_1,cur_pos_3)
+	a_1_3 = odesystem_vel(constant_m1, constant_m2,cur_pos_3,cur_pos_1,cur_pos_2)
+	# Estimating x based on our first approximation of acceleration, k1 in the usual equation for a fourth order Runge-Kutte Method
+	new_vel_est_1 = cur_vel_1 + a_1_1*(delta_t/2)
+	new_vel_est_2 = cur_vel_2 + a_1_2*(delta_t/2)
+	new_vel_est_3 = cur_vel_3 + a_1_3*(delta_t/2)
+	new_pos_est_1 = cur_pos_1 + new_vel_est_1*(delta_t/2)
+	new_pos_est_2 = cur_pos_2 + new_vel_est_2*(delta_t/2)
+	new_pos_est_3 = cur_pos_3 + new_vel_est_3*(delta_t/2)
+	# Finding other estimated accelerations (k2s) for the three objects
+	a_2_1 = odesystem_vel(constant_m2, constant_m3, new_pos_est_1, new_pos_est_2, new_pos_est_3)
+	a_2_2 = odesystem_vel(constant_m1,constant_m3, new_pos_est_2, new_pos_est_1, new_pos_est_3)
+	a_2_3 = odesystem_vel(constant_m1, constant_m2, new_pos_est_3, new_pos_est_1, new_pos_est_2)
+	# Estimating x based on our second approximation of acceleration, k2 in the usual equation for a fourth order Runge-Kutte Method
+	new_vel_est_1 = cur_vel_1 + a_2_1*(delta_t/2)
+	new_vel_est_2 = cur_vel_2 + a_2_2*(delta_t/2)
+	new_vel_est_3 = cur_vel_3 + a_2_3*(delta_t/2)
+	new_pos_est_1 = cur_pos_1 + new_vel_est_1*(delta_t/2)
+	new_pos_est_2 = cur_pos_2 + new_vel_est_2*(delta_t/2)
+	new_pos_est_3 = cur_pos_3 + new_vel_est_3*(delta_t/2)
+	# Finding other estimated accelerations (k3s) for the three objects
+	a_3_1 = odesystem_vel(constant_m2, constant_m3, new_pos_est_1, new_pos_est_2, new_pos_est_3)
+	a_3_2 = odesystem_vel(constant_m1,constant_m3, new_pos_est_2, new_pos_est_1, new_pos_est_3)
+	a_3_3 = odesystem_vel(constant_m1, constant_m2, new_pos_est_3, new_pos_est_1, new_pos_est_2)
+	# Estimating x based on our third approximation of acceleration, k3 in the usual equation for a fourth order Runge-Kutte Method 
+	new_vel_est_1 = cur_vel_1 + a_3_1*(delta_t)
+	new_vel_est_2 = cur_vel_2 + a_3_2*(delta_t)
+	new_vel_est_3 = cur_vel_3 + a_3_3*(delta_t)
+	new_pos_est_1 = cur_pos_1 + new_vel_est_1*(delta_t)
+	new_pos_est_2 = cur_pos_2 + new_vel_est_2*(delta_t)
+	new_pos_est_3 = cur_pos_3 + new_vel_est_3*(delta_t)
+	# Finding other estimated accelerations (k4s) for the three objects
+	a_4_1 = odesystem_vel(constant_m2, constant_m3, new_pos_est_1, new_pos_est_2, new_pos_est_3)
+	a_4_2 = odesystem_vel(constant_m1,constant_m3, new_pos_est_2, new_pos_est_1, new_pos_est_3)
+	a_4_3 = odesystem_vel(constant_m1, constant_m2, new_pos_est_3, new_pos_est_1, new_pos_est_2)
+	# Determining new positions using weighted average of the Runge-Kutte Method
+	new_vel_1 = cur_vel_1+avg_slope(a_1_1, a_2_1,a_3_1, a_4_1)*delta_t
+	new_vel_2 = cur_vel_2+avg_slope(a_1_2, a_2_2,a_3_2, a_4_2)*delta_t
+	new_vel_3 = cur_vel_3+avg_slope(a_1_3, a_2_3,a_3_3, a_4_3)*delta_t
+	new_pos_1 = cur_pos_1 + new_vel_1 * delta_t
+	new_pos_2 = cur_pos_2 + new_vel_2 * delta_t
+	new_pos_3 = cur_pos_3 + new_vel_3 * delta_t
+	current_com = m1*(cur_pos_1) + m2*(cur_pos_2)+m3*(cur_pos_3)
+	current_com
+	cur_col += 1
+	velocities_1[0:3,cur_col] = new_vel_1
+	velocities_2[0:3,cur_col] = new_vel_2
+	velocities_3[0:3,cur_col] = new_vel_3
+	positions_1[0:3,cur_col] = new_pos_1
+	positions_2[0:3,cur_col] = new_pos_2
+	positions_3[0:3,cur_col] = new_pos_3
+	accelerations_1[0:3, cur_col-1] = avg_slope(a_1_1, a_2_1,a_3_1, a_4_1)
+	accelerations_2[0:3, cur_col-1] = avg_slope(a_1_2, a_2_2,a_3_2, a_4_2)
+	accelerations_3[0:3, cur_col-1] = avg_slope(a_1_3, a_2_3,a_3_3, a_4_3)
+	curr_diff = alg.norm(np.subtract(new_pos_3, new_pos_2))
+	print(curr_diff)
+	print(initial_diff)
+	if curr_diff > initial_diff: # The for loop stops when the current difference in position is bigger than the inital difference, as requested. 
+		break
+t = cur_col*delta_t
+time_array = np.linspace(0, t, cur_col, True)
+
+#Plotting the motion of voyager
+fig = plt.figure()
+fig.set_size_inches(9.2, 7.2)
+ax = fig.add_subplot(111, projection="3d")
+plot_mass_2_1, = ax.plot(positions_2[0, :cur_col], positions_2[1, :cur_col], positions_2[2, :cur_col], label="Jupiter", linewidth=1.0, color = '#66b3ff', alpha=0.9)
+plot_mass_3_1, = ax.plot(positions_3[0, :cur_col], positions_3[1, :cur_col], positions_3[2, :cur_col], label="Voyager", linewidth=1.0, color = '#99ff99', alpha=0.9)
+plot_mass_2_2, = ax.plot(positions_2[0, :cur_col], positions_2[1, :cur_col], positions_2[2, :cur_col], 'o', color = '#66b3ff', alpha=0.9)
+plot_mass_3_2, = ax.plot(positions_3[0, :cur_col], positions_3[1, :cur_col], positions_3[2, :cur_col], 'o', color = '#99ff99', alpha=0.9)
+fig.legend()
+ax.set_title("Figure 5b: Voyager's Slingshot around Jupiter", font = cm)
+ax.set_xlabel("x (m)", font = cm)
+ax.set_ylabel("y (m)", font = cm)
+ax.set_zticks([])
+
+def update(frame):
+	ax.view_init(azim = -90, elev = 90)
+	cur_frame_ind = int(((t/delta_t)/frames)*frame)
+	next_ind = int(((t/delta_t)/frames)*frame)+1
+	point_2 = plot_mass_2_2.set_data_3d(positions_2[0,cur_frame_ind:next_ind], positions_2[1,cur_frame_ind:next_ind],positions_2[2,cur_frame_ind:next_ind])
+	point_3 = plot_mass_3_2.set_data_3d(positions_3[0,cur_frame_ind:next_ind], positions_3[1,cur_frame_ind:next_ind],positions_3[2,cur_frame_ind:next_ind])
+	line_2 = plot_mass_2_1.set_data_3d(positions_2[0,:cur_frame_ind], positions_2[1,:cur_frame_ind],positions_2[2,:cur_frame_ind])
+	line_3 = plot_mass_3_1.set_data_3d(positions_3[0,:cur_frame_ind], positions_3[1,:cur_frame_ind],positions_3[2,
+	:cur_frame_ind])
+	return line_2, line_3, point_2, point_3
+
+ani = animation.FuncAnimation(fig, update,frames)
+
+ani.save("Q4 (P36): Jupiter Slingshot Failed.gif", fps = 120)
---
+++ Project/Q4/Q4, m1 __ m2/Q4.py
@@ -0,0 +1,189 @@
+# This code is adapted from my code for a system with three bodies so the set up isn't what is demanded from us by the question doc.
+# If they "collide" then the gravitational force approaches infinity, forcing body 2 and body 3 apart, it is conceivable that they would approach each other after a very long time, but I'm not sure whether it's worth simulating that. 
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as const
+import numpy.linalg as alg
+from time import perf_counter
+from matplotlib import animation
+from pyfonts import load_font
+import matplotlib.font_manager
+
+# Setting font for the plot
+cm = load_font(font_path="/Users/mav/Onedrive/026 Fonts/cm-unicode-0.7.0/cmunrm.ttf")
+
+
+# Defining G the Gravitational Constant
+G = 1
+
+# final time and change in time
+frames = 120
+t = 13
+delta_t = 0.001
+
+
+# Defining a funciton for newton's gravitational laws
+
+t1 = perf_counter()
+
+# Masses in our three body problem
+
+m1 = 0
+m2 = 10
+m3 = 1
+
+
+# Arrays for 1, 2, and 3 positions + defining the intial positions
+
+positions_1 = np.ones([3,int(t/delta_t)+2])
+positions_2 = np.ones([3,int(t/delta_t)+2])
+positions_3 = np.ones([3,int(t/delta_t)+2])
+positions_1[0:3,0] = [0, 0, 0]
+positions_2[0:3,0] = [1, 0, 0]
+positions_3[0:3,0] = [-1, 0, 0]
+
+# Arrays for 1, 2, and 3 velocities + defining the initial velocities
+
+velocities_1 = np.ones([3,int(t/delta_t)+2])
+velocities_2 = np.ones([3,int(t/delta_t)+2])
+velocities_3 = np.ones([3,int(t/delta_t)+2])
+velocities_1[0:3,0] = [0, 0, 0]
+velocities_2[0:3,0] = [0, 0.5, 0]
+velocities_3[0:3,0] = [0, -5, 0]
+
+# Arrays for 1, 2, and 3 accelerations + defining the initial accelerations (if any)
+
+accelerations_1 = np.zeros([3,int(t/delta_t)+1])
+accelerations_2 = np.zeros([3,int(t/delta_t)+1])
+accelerations_3 = np.zeros([3,int(t/delta_t)+1])
+
+# G * m_i are all constants 
+
+# Array for time 
+
+
+time_array = np.linspace(0, t, (int(t/delta_t))+1, True)
+
+
+# We can define constants for everything that doesn't change, particularly  G*m1, G*m2, G*m3
+
+constant_m1 = G*m1
+constant_m2 = G*m2
+constant_m3 = G*m3
+
+# 4th order runge-kutte method for velocity, and position
+
+def avg_slope(k_1, k_2, k_3, k_4):
+	return (k_1 + 2*k_2 + 2*k_3 + k_4)/6
+
+def odesystem_vel(const_1,const_2,posofmass,position_1,position_2):
+	return (const_1/((alg.norm(np.subtract(position_1,posofmass)))**3))*np.subtract(position_1,posofmass)+(const_2/((alg.norm(np.subtract(position_2,posofmass)))**3))*np.subtract(position_2,posofmass)
+
+
+cur_col = 0
+for i in time_array:
+	cur_pos_1 = positions_1[0:3,cur_col]
+	cur_pos_2 = positions_2[0:3,cur_col]
+	cur_pos_3 = positions_3[0:3,cur_col]
+	cur_vel_1 = velocities_1[0:3,cur_col]
+	cur_vel_2 = velocities_2[0:3,cur_col]
+	cur_vel_3 = velocities_3[0:3,cur_col]
+	# Finding estimated accelerations (k1s) for the three objects
+	a_1_1 = odesystem_vel(constant_m2, constant_m3,cur_pos_1,cur_pos_2,cur_pos_3)
+	a_1_2 = odesystem_vel(constant_m1, constant_m3,cur_pos_2,cur_pos_1,cur_pos_3)
+	a_1_3 = odesystem_vel(constant_m1, constant_m2,cur_pos_3,cur_pos_1,cur_pos_2)
+	# Estimating x based on our first approximation of acceleration, k1 in the usual equation for a fourth order Runge-Kutte Method
+	new_vel_est_1 = cur_vel_1 + a_1_1*(delta_t/2)
+	new_vel_est_2 = cur_vel_2 + a_1_2*(delta_t/2)
+	new_vel_est_3 = cur_vel_3 + a_1_3*(delta_t/2)
+	new_pos_est_1 = cur_pos_1 + new_vel_est_1*(delta_t/2)
+	new_pos_est_2 = cur_pos_2 + new_vel_est_2*(delta_t/2)
+	new_pos_est_3 = cur_pos_3 + new_vel_est_3*(delta_t/2)
+	# Finding other estimated accelerations (k2s) for the three objects
+	a_2_1 = odesystem_vel(constant_m2, constant_m3, new_pos_est_1, new_pos_est_2, new_pos_est_3)
+	a_2_2 = odesystem_vel(constant_m1,constant_m3, new_pos_est_2, new_pos_est_1, new_pos_est_3)
+	a_2_3 = odesystem_vel(constant_m1, constant_m2, new_pos_est_3, new_pos_est_1, new_pos_est_2)
+	# Estimating x based on our second approximation of acceleration, k2 in the usual equation for a fourth order Runge-Kutte Method
+	new_vel_est_1 = cur_vel_1 + a_2_1*(delta_t/2)
+	new_vel_est_2 = cur_vel_2 + a_2_2*(delta_t/2)
+	new_vel_est_3 = cur_vel_3 + a_2_3*(delta_t/2)
+	new_pos_est_1 = cur_pos_1 + new_vel_est_1*(delta_t/2)
+	new_pos_est_2 = cur_pos_2 + new_vel_est_2*(delta_t/2)
+	new_pos_est_3 = cur_pos_3 + new_vel_est_3*(delta_t/2)
+	# Finding other estimated accelerations (k3s) for the three objects
+	a_3_1 = odesystem_vel(constant_m2, constant_m3, new_pos_est_1, new_pos_est_2, new_pos_est_3)
+	a_3_2 = odesystem_vel(constant_m1,constant_m3, new_pos_est_2, new_pos_est_1, new_pos_est_3)
+	a_3_3 = odesystem_vel(constant_m1, constant_m2, new_pos_est_3, new_pos_est_1, new_pos_est_2)
+	# Estimating x based on our third approximation of acceleration, k3 in the usual equation for a fourth order Runge-Kutte Method 
+	new_vel_est_1 = cur_vel_1 + a_3_1*(delta_t)
+	new_vel_est_2 = cur_vel_2 + a_3_2*(delta_t)
+	new_vel_est_3 = cur_vel_3 + a_3_3*(delta_t)
+	new_pos_est_1 = cur_pos_1 + new_vel_est_1*(delta_t)
+	new_pos_est_2 = cur_pos_2 + new_vel_est_2*(delta_t)
+	new_pos_est_3 = cur_pos_3 + new_vel_est_3*(delta_t)
+	# Finding other estimated accelerations (k4s) for the three objects
+	a_4_1 = odesystem_vel(constant_m2, constant_m3, new_pos_est_1, new_pos_est_2, new_pos_est_3)
+	a_4_2 = odesystem_vel(constant_m1,constant_m3, new_pos_est_2, new_pos_est_1, new_pos_est_3)
+	a_4_3 = odesystem_vel(constant_m1, constant_m2, new_pos_est_3, new_pos_est_1, new_pos_est_2)
+	# Determining new positions using weighted average of the Runge-Kutte Method
+	new_vel_1 = cur_vel_1+avg_slope(a_1_1, a_2_1,a_3_1, a_4_1)*delta_t
+	new_vel_2 = cur_vel_2+avg_slope(a_1_2, a_2_2,a_3_2, a_4_2)*delta_t
+	new_vel_3 = cur_vel_3+avg_slope(a_1_3, a_2_3,a_3_3, a_4_3)*delta_t
+	new_pos_1 = cur_pos_1 + new_vel_1 * delta_t
+	new_pos_2 = cur_pos_2 + new_vel_2 * delta_t
+	new_pos_3 = cur_pos_3 + new_vel_3 * delta_t
+	cur_col += 1
+	velocities_1[0:3,cur_col] = new_vel_1
+	velocities_2[0:3,cur_col] = new_vel_2
+	velocities_3[0:3,cur_col] = new_vel_3
+	positions_1[0:3,cur_col] = new_pos_1
+	positions_2[0:3,cur_col] = new_pos_2
+	positions_3[0:3,cur_col] = new_pos_3
+	accelerations_1[0:3, cur_col-1] = avg_slope(a_1_1, a_2_1,a_3_1, a_4_1)
+	accelerations_2[0:3, cur_col-1] = avg_slope(a_1_2, a_2_2,a_3_2, a_4_2)
+	accelerations_3[0:3, cur_col-1] = avg_slope(a_1_3, a_2_3,a_3_3, a_4_3)
+
+momentum_2 = m2*velocities_2
+momentum_3 = m3*velocities_3
+
+# Plotting - for the momentum plot.
+fig_2 = plt.figure()
+plt.title("Figure 4: The Momenta of Both Bodies, with one far heavier than the other", font = cm)
+plt.xlabel("Time (s)", font = cm)
+plt.ylabel("Speed (kgm/s)", font = cm)
+plt.plot(time_array, momentum_2[0, 1:]+momentum_2[1, 1:], label = "Momentum of Body 2")
+plt.plot(time_array, momentum_3[0, 1:]+momentum_3[1, 1:], label = "Momentum of Body 3")
+plt.plot(time_array, momentum_2[0, 1:]+momentum_3[0, 1:]+momentum_3[1, 1:]+momentum_2[1, 1:], label = "Total Momentum of System")
+plt.legend()
+plt.savefig("Figure 4.svg")
+plt.show()
+
+
+# Plotting for the gif of the bodies orbiting each other stably. 
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+plot_mass_2_1, = ax.plot(positions_2[0], positions_2[1], positions_2[2], label="Body 2", linewidth=1.0, color = '#66b3ff', alpha=0.9)
+plot_mass_3_1, = ax.plot(positions_3[0], positions_3[1], positions_3[2], label="Body 3", linewidth=1.0, color = '#99ff99', alpha=0.9)
+plot_mass_2_2, = ax.plot(positions_2[0], positions_2[1], positions_2[2], 'o', color = '#66b3ff', alpha=0.9)
+plot_mass_3_2, = ax.plot(positions_3[0], positions_3[1], positions_3[2], 'o', color = '#99ff99', alpha=0.9)
+fig.legend()
+ax.set_title("Figure 4: Stable System of Two Bodies Orbiting Each Other", font = cm)
+ax.set_xlabel("x (m)", font = cm)
+ax.set_ylabel("y (m)", font = cm)
+ax.set_zlabel("z (m)",font = cm)
+
+
+def update(frame):
+	cur_frame_ind = int(((t/delta_t)/frames)*frame)
+	next_ind = cur_frame_ind+1
+	point_2 = plot_mass_2_2.set_data_3d(positions_2[0,cur_frame_ind:next_ind], positions_2[1,cur_frame_ind:next_ind],positions_2[2,cur_frame_ind:next_ind])
+	point_3 = plot_mass_3_2.set_data_3d(positions_3[0,cur_frame_ind:next_ind], positions_3[1,cur_frame_ind:next_ind],positions_3[2,cur_frame_ind:next_ind])
+	line_2 = plot_mass_2_1.set_data_3d(positions_2[0,:cur_frame_ind], positions_2[1,:cur_frame_ind],positions_2[2,:cur_frame_ind])
+	line_3 = plot_mass_3_1.set_data_3d(positions_3[0,:cur_frame_ind], positions_3[1,:cur_frame_ind],positions_3[2,:cur_frame_ind])
+	return line_2, line_3, point_2, point_3
+
+ani = animation.FuncAnimation(fig, update,frames)
+
+
+ani.save("Q4.gif", fps = 120)
